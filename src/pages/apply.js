3380008b0add864a9288da4fe0dc3d80e5939790
Hans Brough <hans.brough@yahoo.com>
2022-12-19 18:45:19 -0800

    removed pricing, simplified availability legend

--- src/pages/apply.js
+++ src/pages/apply.js
@@ -62,7 +62,7 @@ const ApplyPage = ({data}) => {
         Sourcing plants for a landscape project can be difficult.
         If you are a contractor, designer, architect or other landscape professional we can help you locate plant materials either with stock on hand or by coordinating with another wholesaler.
       </p>
-      <p>To apply please use the form below to describe your business and project needs.</p>
+      <p>To apply please use the form below to describe your business and project needs. We will respond within a few business days and share an availability list with pricing.</p>
 
       <form
           onSubmit={(e) => handleSubmit(e)}
@@ -148,11 +148,12 @@ const ApplyPage = ({data}) => {
           </Button>
       </form>
 
-      <Img
+      {/*<Img
           fixed={data.nurseryHero.childImageSharp.fixed}
           alt="Plants growing in all day sun."
           className="aloe-regions-map"
         />
+        */}
     </Layout>
   )
 }
